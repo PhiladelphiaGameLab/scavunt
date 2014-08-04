23797b501358c583405dd1b9fd8cfa7378571d26
Aaron Segal <aaron@philadelphiagamelab.org>
2014-08-04 10:23:22 -0400

    updating hardcode in Storybuilder for new task code

--- app/src/main/java/org/philadelphiagamelab/scavunt/StoryBuilder.java
+++ app/src/main/java/org/philadelphiagamelab/scavunt/StoryBuilder.java
@@ -15,8 +15,8 @@ public class StoryBuilder {
 
         clusters = new ArrayList<EventCluster>();
 
-        Task event1Video = new Task("video", Task.Type.RECEIVE_VIDEO, R.raw.test_video, true);
-        Task event1Text = new Task("text", Task.Type.RECEIVE_TEXT, R.string.testString, true);
+        Task event1Video = new Task("video", Task.Type.RECEIVE_VIDEO, R.raw.test_video, 0);
+        Task event1Text = new Task("text", Task.Type.RECEIVE_TEXT, R.string.testString, 0);
         ArrayList<Task> event1Tasks = new ArrayList<Task>();
         event1Tasks.add(event1Video);
         event1Tasks.add(event1Text);
@@ -25,7 +25,7 @@ public class StoryBuilder {
         event1.getLocation().setLatitude(39.952472);
         event1.getLocation().setLongitude(-75.165297);
 
-        Task event2Audio = new Task("audio", Task.Type.RECEIVE_AUDIO, R.raw.space_cruise, true);
+        Task event2Audio = new Task("audio", Task.Type.RECEIVE_AUDIO, R.raw.space_cruise, 0);
         ArrayList<Task> event2Tasks = new ArrayList<Task>();
         event2Tasks.add(event2Audio);
         ArrayList<Task> event2TasksToComplete = event2Tasks;
@@ -33,7 +33,7 @@ public class StoryBuilder {
         event2.getLocation().setLatitude(39.952730);
         event2.getLocation().setLongitude(-75.166932);
 
-        Task event3Text = new Task("text", Task.Type.RECEIVE_TEXT, R.string.testString, true);
+        Task event3Text = new Task("text", Task.Type.RECEIVE_TEXT, R.string.testString, 0);
         ArrayList<Task> event3Tasks = new ArrayList<Task>();
         event3Tasks.add(event3Text);
         ArrayList<Task> event3TasksToComplete = event3Tasks;
@@ -41,8 +41,8 @@ public class StoryBuilder {
         event3.getLocation().setLatitude(39.951329);
         event3.getLocation().setLongitude(-75.167129);
 
-        Task event4Image1 = new Task("image", Task.Type.RECEIVE_IMAGE, R.drawable.ic_launcher, true);
-        Task event4Image2 = new Task("image", Task.Type.RECEIVE_IMAGE, R.drawable.ic_launcher, true);
+        Task event4Image1 = new Task("image", Task.Type.RECEIVE_IMAGE, R.drawable.ic_launcher, 0);
+        Task event4Image2 = new Task("image", Task.Type.RECEIVE_IMAGE, R.drawable.ic_launcher, 0);
         ArrayList<Task> event4Tasks = new ArrayList<Task>();
         event4Tasks.add(event4Image1);
         event4Tasks.add(event4Image2);
