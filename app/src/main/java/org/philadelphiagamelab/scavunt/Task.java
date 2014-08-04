d28b6614c0ba8dcb273af6e2af8da9d9b788a3d9
Aaron Segal <aaron@philadelphiagamelab.org>
2014-08-04 10:19:31 -0400

    working on task delay and completion

--- app/src/main/java/org/philadelphiagamelab/scavunt/Task.java
+++ app/src/main/java/org/philadelphiagamelab/scavunt/Task.java
@@ -14,54 +14,48 @@ public class Task {
     private String title;
     private Type type;
     private int resourceID;
+    private long delay;
     private boolean visible;
     private boolean complete;
 
-    public Task (String titleIn, Type typeIn, int resourceIDIn, boolean visibleIn,
+    public Task (String titleIn, Type typeIn, int resourceIDIn, long delayIn,
                  boolean completeIn) {
         this.title = titleIn;
         this.type = typeIn;
         this.resourceID = resourceIDIn;
-        this.visible = visibleIn;
         this.complete = completeIn;
+        this.delay = delayIn;
+        if( delay <= 0) {
+            this.visible = true;
+        }
+        else {
+            this.visible = false;
+            // TODO: implement visibility delay
+        }
     }
 
-    public Task (String titleIn, Type typeIn, int resourceIDIn, boolean visibleIn) {
+    public Task (String titleIn, Type typeIn, int resourceIDIn, long delayIn) {
         this.title = titleIn;
         this.type = typeIn;
         this.resourceID = resourceIDIn;
-        this.visible = visibleIn;
-        this.complete = false;
+        this.complete = true;
+        this.delay = delayIn;
+        if( delay <= 0) {
+            this.visible = true;
+        }
+        else {
+            this.visible = false;
+            // TODO: implement visibility delay
+        }
     }
 
     public Task (String titleIn, Type typeIn, int resourceIDIn) {
         this.title = titleIn;
         this.type = typeIn;
         this.resourceID = resourceIDIn;
-        this.visible = false;
-        this.complete = false;
-    }
-
-    public Task (String titleIn, Type typeIn, boolean visibleIn, boolean completeIn) {
-        this.title = titleIn;
-        this.type = typeIn;
-        this.visible = visibleIn;
-        this.complete = completeIn;
-    }
-
-
-    public Task (String titleIn, Type typeIn, boolean visibleIn) {
-        this.title = titleIn;
-        this.type = typeIn;
-        this.visible = visibleIn;
-        this.complete = false;
-    }
-
-    public Task (String titleIn, Type typeIn) {
-        this.title = titleIn;
-        this.type = typeIn;
-        this.visible = false;
-        this.complete = false;
+        this.delay = 0;
+        this.visible = true;
+        this.complete = true;
     }
 
     public String getTitle() {
