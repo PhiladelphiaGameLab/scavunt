fa9334a772f9845a32bbe728536a66f12eb40ba8
donxu29 <donxu29@gmail.com>
2014-08-04 14:06:48 -0400

    ImageFrg modified

--- app/src/main/java/org/philadelphiagamelab/scavunt/AudioFragment.java
+++ app/src/main/java/org/philadelphiagamelab/scavunt/AudioFragment.java
@@ -1,64 +1,55 @@
 package org.philadelphiagamelab.scavunt;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ImageFragment.OnFragmentInteractionListener} interface
+ * {@link AudioFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ImageFragment#newInstance} factory method to
+ * Use the {@link AudioFragment#newInstance} factory method to
  * create an instance of this fragment.
  *
  */
-public class ImageFragment extends Fragment {
-    private static final String ARG_PARAM1 = "imageResourceID";
+public class AudioFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "audioResourceID";
     private static final String ARG_PARAM2 = "layoutResourceID";
 
-    private Task toRepresent;
-    private int imageResourceID;
+    private int audioResourceID;
     private int layoutResourceID;
 
+    private MediaPlayer mPlayer;
     private OnFragmentInteractionListener mListener;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param imageResourceIDIn id for the image.
-     * @param layoutResourceIDIn id for the layout.
-     * @return A new instance of fragment ImageFragment.
+     * @return A new instance of fragment AudioFragment.
      */
-
-    public static ImageFragment newInstance(int imageResourceIDIn, int layoutResourceIDIn) {
-        ImageFragment fragment = new ImageFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_PARAM1, imageResourceIDIn);
-        args.putInt(ARG_PARAM2, layoutResourceIDIn);
-        fragment.setArguments(args);
+    // TODO: Rename and change types and number of parameters
+    public static AudioFragment newInstance() {
+        AudioFragment fragment = new AudioFragment();
         return fragment;
     }
-    public ImageFragment() {}
+    public AudioFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (savedInstanceState != null){
-            imageResourceID = savedInstanceState.getInt(ARG_PARAM1);
-            layoutResourceID = savedInstanceState.getInt(ARG_PARAM2);
-        }
         if (getArguments() != null) {
-            imageResourceID = getArguments().getInt(ARG_PARAM1);
+            audioResourceID = getArguments().getInt(ARG_PARAM1);
             layoutResourceID = getArguments().getInt(ARG_PARAM2);
         }
     }
@@ -67,13 +58,15 @@ public class ImageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(layoutResourceID, container, false);
-        ImageView imageView = (ImageView) view.findViewById(R.id.imageView_imageFrag);
-        imageView.setImageResource(imageResourceID);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_audio, container, false);
     }
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -92,20 +85,6 @@ public class ImageFragment extends Fragment {
         mListener = null;
     }
 
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putInt(ARG_PARAM1, imageResourceID);
-        outState.putInt(ARG_PARAM2, layoutResourceID);
-
-        super.onSaveInstanceState(outState);
-    }
-
-    public void updateTask(Task toRepresentIn) {
-        toRepresent = toRepresentIn;
-        layoutResourceID = toRepresent.getLayoutID();
-        imageResourceID = toRepresent.getResourceID();
-    }
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -118,7 +97,7 @@ public class ImageFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
+        public void onFragmentInteraction(Uri uri);
     }
 
 }
